2e98d4631cb53317f52e37d0c6d374e5178dd9fc
전해성 <seong0428@gmail.com>
2019-12-19 01:13:16 +0900

    Initial commit

---
+++ src/main/java/com/depromeet/todo/TodoApplication.java
@@ -0,0 +1,13 @@
+package com.depromeet.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoApplication.class, args);
+	}
+
+}
