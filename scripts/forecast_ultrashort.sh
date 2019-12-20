f0c5529af68c77a8138d58b72ac553992656c428
전해성 <seong0428@gmail.com>
2019-12-20 23:14:54 +0900

    feat #2 - 날씨 api 테스트용 스크립트를 추가합니다

---
+++ scripts/forecast_ultrashort.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+# 초단기예보조회.
+# 초단기예보 정보를 조회하기 위해 
+# 예보일자, 예보시간, 예보지점X좌표, 예보지점Y좌표의 조회 조건으로 
+# 자료구분코드, 예보값,예보일자, 예보시간, 예보지점X좌표,예보지점Y좌표의 정보를 조회하는 기능	
+
+
+# validate
+if [ "$#" -lt "5" ]; then
+  echo "Usage: forecast_ultrashort.sh {SERVICE_KEY} {BASE_DATE} {BASE_TIME} {NX} {NY} {PAGE=1} {SIZE=10}" 
+  exit 1
+fi
+
+
+# resolve parameters
+SERVICE_KEY=$1
+BASE_DATE=$2
+BASE_TIME=$3
+NX=$4
+NY=$5
+PAGE=1
+if [ -n "$6" ]; then
+  PAGE=$6
+fi
+SIZE=10
+if [ -n "$7" ]; then
+  SIZE=$7
+fi
+
+
+# print 
+echo "SERVICE_KEY: ${SERVICE_KEY}"
+echo "BASE_DATE: ${BASE_DATE}"
+echo "BASE_TIME: ${BASE_TIME}"
+echo "NX: ${NX}"
+echo "NY: ${NY}"
+echo "PAGE: ${PAGE}"
+echo "SIZE: ${SIZE}"
+
+
+# config
+REQUEST_URL=http://newsky2.kma.go.kr/service/SecndSrtpdFrcstInfoService2/ForecastTimeData
+
+
+# execute
+curl -s "${REQUEST_URL}?ServiceKey=${SERVICE_KEY}&base_date=${BASE_DATE}&base_time=${BASE_TIME}&nx=${NX}&ny=${NY}&numOfRows=${SIZE}&pageNo=${PAGE}&_type=json" | jq
+
