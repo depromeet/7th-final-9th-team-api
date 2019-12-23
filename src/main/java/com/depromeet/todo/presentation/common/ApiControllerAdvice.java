4ec5474b3d4616acbb53a4c1a92bad1d0afba689
전해성 <seong0428@gmail.com>
2019-12-24 02:38:17 +0900

    feat #6 - 예외처리 기능을 추가합니다

---
+++ src/main/java/com/depromeet/todo/presentation/common/ApiControllerAdvice.java
@@ -0,0 +1,49 @@
+package com.depromeet.todo.presentation.common;
+
+import com.depromeet.todo.application.ExternalServiceException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class ApiControllerAdvice {
+    /**
+     * 사용자 요청에 오류가 있는 경우
+     */
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiResponse handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        log.error("Exception occurred while handling arguments", ex);
+        return ApiResponse.failureFrom(
+                ex.getMessage()
+        );
+    }
+
+    /**
+     * 외부 서비스를 사용할 수 없는 경우
+     */
+    @ExceptionHandler(ExternalServiceException.class)
+    @ResponseStatus(HttpStatus.SERVICE_UNAVAILABLE)
+    public ApiResponse handleExternalServiceUnavailable(ExternalServiceException ex) {
+        log.error("Exception occurred while using External service", ex);
+        return ApiResponse.failureFrom(
+                ex.getMessage()
+        );
+    }
+
+    /**
+     * 서버 내부 오류
+     */
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ApiResponse handleException(Exception ex) {
+        log.error("Exception occurred", ex);
+        return ApiResponse.failureFrom(
+                ex.getMessage()
+        );
+    }
+}
