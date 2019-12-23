ca6816c64f3db38d795f16db5d69787ef8cdd856
전해성 <seong0428@gmail.com>
2019-12-24 03:28:47 +0900

    feat #6 - 기상청 API 를 직접 요청하는 테스트를 제외합니다

--- src/main/java/com/depromeet/todo/infrastructure/kma/KmaApiBodyItems.java
+++ src/main/java/com/depromeet/todo/infrastructure/kma/KmaApiBodyItems.java
@@ -2,11 +2,19 @@ package com.depromeet.todo.infrastructure.kma;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.util.Collections;
 import java.util.List;
 
 @Data
+@NoArgsConstructor
 public class KmaApiBodyItems<T extends KmaApiWeatherItem> {
     @JsonProperty("item")
     private List<T> itemList;
+
+    // XXX: items: "" 로 들어오는 경우에 대한 방어코드
+    public KmaApiBodyItems(String input) {
+        this.itemList = Collections.emptyList();
+    }
 }
--- src/test/java/com/depromeet/todo/integration/KmaWeatherServiceIntegrationTest.java
+++ src/test/java/com/depromeet/todo/integration/KmaWeatherServiceIntegrationTest.java
@@ -4,6 +4,7 @@ import com.depromeet.todo.domain.location.GcsLocation;
 import com.depromeet.todo.domain.location.PcsLocation;
 import com.depromeet.todo.domain.weather.Weather;
 import com.depromeet.todo.domain.weather.WeatherService;
+import jdk.nashorn.internal.ir.annotations.Ignore;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,6 +13,7 @@ import java.time.LocalDateTime;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Ignore // 직접 기상청 api 를 호출하는 테스트이므로, 평소에는 ignore 해둡니다
 @SuppressWarnings("NonAsciiCharacters")
 @SpringBootTest
 class KmaWeatherServiceIntegrationTest {
@@ -20,19 +22,19 @@ class KmaWeatherServiceIntegrationTest {
 
     @Test
     void 초단기실황조회_pcs_좌표() {
-        // given
-        // when
-        Weather weather = KmaWeatherService.getCurrentWeather(PcsLocation.of(63, 127), LocalDateTime.now());
-        // then
+        Weather weather = KmaWeatherService.getCurrentWeather(
+                PcsLocation.of(63, 127),
+                LocalDateTime.now()
+        );
         assertThat(weather).isNotNull();
     }
 
     @Test
     void 초단기실황조회_gcs_좌표() {
-        // given
-        // when
-        Weather weather = KmaWeatherService.getCurrentWeather(GcsLocation.of(37.4876095, 126.9833778), LocalDateTime.now());
-        // then
+        Weather weather = KmaWeatherService.getCurrentWeather(
+                GcsLocation.of(37.4876095, 126.9833778),
+                LocalDateTime.now()
+        );
         assertThat(weather).isNotNull();
     }
 }
