7ff61a42205c9353fd382219eaf8f2feac11b2dd
전해성 <seong0428@gmail.com>
2019-12-27 03:01:48 +0900

    feat #19 - 인증 성공시 accessToken 을 생성합니다

--- src/main/java/com/depromeet/todo/infrastructure/spring/security/JsonAuthenticationSuccessHandler.java
+++ src/main/java/com/depromeet/todo/infrastructure/spring/security/JsonAuthenticationSuccessHandler.java
@@ -1,5 +1,6 @@
 package com.depromeet.todo.infrastructure.spring.security;
 
+import com.depromeet.todo.application.TokenService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -15,14 +16,21 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class JsonAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
     private final ObjectMapper writeObjectMapper;
+    private final TokenService<Long> tokenService;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request,
                                         HttpServletResponse response,
                                         Authentication authentication) throws IOException, ServletException {
+
+        TodoAuthenticationToken todoAuthenticationToken = (TodoAuthenticationToken) authentication;
+        String accessToken = tokenService.generate(
+                todoAuthenticationToken.getMemberId()
+        );
+
         response.setStatus(HttpStatus.OK.value());
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-        // TODO: 토큰발급해주기
-        writeObjectMapper.writeValue(response.getOutputStream(), "todoAccessToken");
+        // TODO: 공통 응답 모듈 머지되면 payload 수정해야함
+        writeObjectMapper.writeValue(response.getOutputStream(), accessToken);
     }
 }
