41d720b8a7e605af39d11cd3d6d45f7ecbaa8772
전해성 <seong0428@gmail.com>
2019-12-27 22:49:27 +0900

    refactor #8 - 중복되는 코드를 제거합니다

--- src/main/java/com/depromeet/todo/infrastructure/kakao/KakaoApiFailedException.java
+++ src/main/java/com/depromeet/todo/infrastructure/kakao/KakaoApiFailedException.java
@@ -1,6 +1,6 @@
 package com.depromeet.todo.infrastructure.kakao;
 
-import com.depromeet.todo.domain.ExternalServiceException;
+import com.depromeet.todo.application.ExternalServiceException;
 
 public class KakaoApiFailedException extends ExternalServiceException {
     public KakaoApiFailedException(String message) {
