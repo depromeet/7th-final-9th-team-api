ab27716e89ab358e8c693c4f3be0c8dc65246ed3
전해성 <seong0428@gmail.com>
2020-01-29 19:42:51 +0900

    fix #37 - allowed origin 에 * 을 넣지 않습니다

--- src/main/java/com/depromeet/todo/infrastructure/spring/WebMvcConfig.java
+++ src/main/java/com/depromeet/todo/infrastructure/spring/WebMvcConfig.java
@@ -11,7 +11,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("*")
+                .allowedOrigins("http://localhost:3000")
                 .allowedMethods(
                         HttpMethod.OPTIONS.name(),
                         HttpMethod.HEAD.name(),
