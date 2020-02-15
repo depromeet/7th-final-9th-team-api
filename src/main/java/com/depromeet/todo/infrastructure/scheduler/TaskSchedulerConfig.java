10eac4ee6de30e09ecbab1dd964fcf00221ddd2f
Juyoung <juy5790@outlook.com>
2020-02-15 18:17:11 +0900

    refactor 매일 자정에 미완료 할일제거

--- src/main/java/com/depromeet/todo/infrastructure/scheduler/TaskSchedulerConfig.java
+++ src/main/java/com/depromeet/todo/infrastructure/scheduler/TaskSchedulerConfig.java
@@ -1,7 +1,9 @@
 package com.depromeet.todo.infrastructure.scheduler;
 
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.stereotype.Component;
 
+@Component
 @EnableScheduling
 public class TaskSchedulerConfig {
 }
--- src/main/java/com/depromeet/todo/presentation/tasks/TaskCleaner.java
+++ src/main/java/com/depromeet/todo/presentation/tasks/TaskCleaner.java
@@ -3,27 +3,33 @@ package com.depromeet.todo.presentation.tasks;
 import com.depromeet.todo.application.tasks.TasksApplicationService;
 import com.depromeet.todo.domain.task.Tasks;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 
 @Slf4j
 @Component
+@ConditionalOnProperty(value = "task-cleaner.execute", havingValue = "true")
 public class TaskCleaner {
 
     private final TasksApplicationService tasksApplicationService;
 
     public TaskCleaner(TasksApplicationService tasksApplicationService) {
-        log.info("마감일 지난 할일정리 = ON");
         this.tasksApplicationService = tasksApplicationService;
     }
 
-    @Scheduled(cron = "0 0 0 * * *")
-    private void executeImpleteTask() {
-        LocalDateTime now = LocalDateTime.now();
+    @Scheduled(cron = "${task-cleaner.cron}")
+    public void execute() {
+        LocalDateTime now = LocalDate.now()
+                                     .atTime(LocalTime.MAX);
         List<Tasks> tasks = tasksApplicationService.changeCompleteTaskOverDeadline(now);
-        log.info("미완료한 할일 {}개 정리: {}", tasks.size(), tasks);
+        log.info("미완료한 할일 {}개 정리: {}", tasks.size(), tasks.stream()
+                                                          .map(Tasks::getId)
+                                                          .toArray());
     }
 }
